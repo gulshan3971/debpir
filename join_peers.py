db4ba4b95e9b9b0d0a545b1664b87a418e693e18
gulshan3971 <35025548+gulshan3971@users.noreply.github.com>
2025-03-06 11:55:10 +0530

    Update join_peers.py

--- join_peers.py
+++ join_peers.py
@@ -1,1 +1,3 @@
-Python script for peers joining the channel.
+cli = Client(net_profile="network_config/network.yaml")
+cli.new_channel("debpirchannel")
+cli.peer_join_channel("peer0.org1.example.com", "debpirchannel")
