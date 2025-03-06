7fbb655dbca9b7331f265a8cb25fbdfe4ba1096f
gulshan3971 <35025548+gulshan3971@users.noreply.github.com>
2025-03-06 11:54:48 +0530

    Update enroll_admin.py

--- enroll_admin.py
+++ enroll_admin.py
@@ -1,1 +1,5 @@
-Python script for enrolling admin users.
+from hfc.fabric import Client
+
+cli = Client(net_profile="network_config/network.yaml")
+cli.new_channel("debpirchannel")
+cli.enroll("Admin", "adminpw", "ca.org1.example.com")
